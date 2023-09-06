e2e3234cfa22c179a344a7d296a57c3f670c72b8
Regina <xzregina14@gmail.com>
2023-09-05 23:01:19 -0700

    Creating the UI - Question Tracker Composable

--- app/src/main/java/com/example/jettriviaapp/components/Questions.kt
+++ app/src/main/java/com/example/jettriviaapp/components/Questions.kt
@@ -1,8 +1,27 @@
 package com.example.jettriviaapp.components
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jettriviaapp.screens.QuestionViewModel
+import com.example.jettriviaapp.util.AppColors
 
 @Composable
 fun Questions(viewModel: QuestionViewModel) {
@@ -15,4 +34,49 @@ fun Questions(viewModel: QuestionViewModel) {
         }
     }
     Log.d("SIZE", "Questions: ${questions?.size}")
+}
+
+@Composable
+@Preview
+fun QuestionDisplay() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(4.dp),
+        color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionsTracker()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun QuestionsTracker(counter: Int = 10, outOf: Int = 100) {
+    Text(text = buildAnnotatedString {
+        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+            withStyle(
+                style = SpanStyle(
+                    color = AppColors.mLightPurple, fontWeight = FontWeight.Bold, fontSize = 27.sp
+                )
+            ) {
+                append("Questions: ${counter}/")
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 14.sp
+                    )
+                ) {
+                    append("$outOf")
+                }
+            }
+        }
+    }, modifier = Modifier.padding(20.dp))
 }
